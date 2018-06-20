2cfcb4abf9dfde41d7f71a24f1afe0d47da89b9e
Joseph Reeve <joseph.reeve@googlemail.com>
2018-06-20 10:56:11 +0100

    Major progress, now able to provide coords

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const fs = require('fs');
 const grib2json = require('grib2json').default;
 const Jimp = require("jimp");
 
-const proj4 = require('proj4');
-
-
-
-
-
-
-
 // Print the v-component of wind at 10 meters above surface
 // Includes header names and data
 grib2json('./gfs.t06z.pgrb2.0p25.f038', {
@@ -25,41 +17,60 @@ grib2json('./gfs.t06z.pgrb2.0p25.f038', {
     const header = json[0].header;
     const data = json[0].data;
 
+
     const keyedValues = {};
     data.forEach((val, index) => {
         const row = Math.floor(index / header.nx);
-        const xCoord = header.lo1 + (header.dx * (index % header.nx)) - (header.lo2 / 2);
+        const xCoord = header.lo1 + (header.dx * (index % header.nx))
         const yCoord = header.la1 - (row * header.dy);
 
-        keyedValues[xCoord] = keyedValues[xCoord] || {};
-        keyedValues[xCoord][yCoord] = {
+        const newXCoord = ((xCoord + 180) % 360) - 180;
+
+        keyedValues[newXCoord] = keyedValues[newXCoord] || {};
+        keyedValues[newXCoord][yCoord] = {
             value: val,
             index: index
         };
     });
-    // console.log(acc);
-    const bottomLeft = [-121.6350, 9.9904];
-    const topRight = [-84.2884, 35.5851];
 
-    const slicedIndexes = [];
-    const sliced = Object.keys(keyedValues).filter(xKey => {
-        const x = Number(xKey);
-        return x > bottomLeft[0] && x < topRight[0];
-    }).reduce((acc, xKey, index) => {
-        slicedIndexes[index] = slicedIndexes[index] || [];
-        acc[xKey] = Object.keys(keyedValues[xKey])
-            .filter(yKey => {
-                const y = Number(yKey);
-                return y < topRight[1] && y > bottomLeft[1];
-            }).reduce((acc, yKey) => {
-                acc[yKey] = keyedValues[xKey][yKey];
-                slicedIndexes[index].push(acc[yKey].index);
-                return acc;
-            }, {});
-        return acc;
-    }, {});
 
-    console.log(sliced, slicedIndexes);
+    const topLeft = [-34, 44];
+    const bottomRight = [54, -45];
+
+    const slicedValues = [];
+    const sliced = Object.keys(keyedValues)
+        .map(x => Number(x))
+        .filter(x => {
+            return x >= topLeft[0] && x <= bottomRight[0];
+        })
+        .sort((a, b) => a == b ? 0 : a > b ? 1 : -1)
+        .reduce((acc, xKey, index) => {
+            slicedValues[index] = slicedValues[index] || [];
+            acc[xKey] = Object.keys(keyedValues[xKey])
+                .map(y => Number(y))
+                .filter(y => {
+                    return y <= topLeft[1] && y >= bottomRight[1];
+                })
+                .sort((a, b) => a == b ? 0 : a < b ? 1 : -1)
+                .reduce((acc, yKey) => {
+                    acc[yKey] = keyedValues[xKey][yKey];
+                    slicedValues[index].push(acc[yKey].value);
+                    return acc;
+                }, {});
+            return acc;
+        }, {});
+
+    const grid = [...Array(header.ny)].map((_, index) => data.slice(index * header.nx, (index + 1) * header.nx));
+
+    const xIndex = Math.round(Math.abs(topLeft[0] - header.lo1) / header.dx);
+    const yIndex = Math.round(Math.abs(topLeft[1] - header.la1) / header.dy);
+
+    const xIndex2 = Math.round(Math.abs(bottomRight[0] - header.lo1) / header.dx);
+    const yIndex2 = Math.round(Math.abs(bottomRight[1] - header.la1) / header.dy);
+
+    console.log(`xIndex: ${xIndex}, yIndex: ${yIndex}`);
+
+    const newGrid = grid.slice(yIndex, yIndex2).map(r => r.slice(xIndex, xIndex2));
 
     // const grid = data.
 
@@ -88,21 +99,40 @@ grib2json('./gfs.t06z.pgrb2.0p25.f038', {
     // console.log(`Header:`, header);
     // console.log(`CoordData:`, coordData);
 
+    // console.log(`xlength: ${newGrid[0].length}/${header.nx} ylength: ${newGrid.length}/${header.ny}`);
 
-
-    const image = new Jimp(header.nx, header.ny, function (err, image) {
+    // const image = new Jimp(slicedIndexes.length, slicedIndexes[0].length, function (err, image) {
+    const image = new Jimp(slicedValues.length, slicedValues[0].length, function (err, image) {
         image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
             // var red = this.bitmap.data[idx + 0];
             // var green = this.bitmap.data[idx + 1];
             // var blue = this.bitmap.data[idx + 2];
             // var alpha = this.bitmap.data[idx + 3];
 
-            this.bitmap.data[idx + 3] = 200;
-            const dataIndex = x + (y * header.nx);
+            if (this.bitmap.data[idx + 3] === 200) {
+                console.log('already have value:', this.bitmap.data[idx + 3]);
+            }
 
-            this.bitmap.data[idx + 0] = Math.abs(range + ((data[dataIndex] - 272.15) * mod));
+            this.bitmap.data[idx + 3] = 200;
+            // const dataIndex = slicedIndexes[x][y];
+            // const dataIndex = x + (y * header.nx);
+
+            // const rawLon = header.lo1 + (header.dx * (x % header.nx));
+
+            // const lon = ((rawLon + 180) % 360) - 180;
+            // const lat = header.la1 - (y * header.dy);
+
+            // if (lon >= topLeft[0] && lon <= bottomRight[0] && lat <= topLeft[1] && lat >= bottomRight[1]) {
+            // const value = keyedValues[lon][lat].value;
+            const value = slicedValues[x][y];
+            // this.bitmap.data[idx + 0] = Math.abs(range + ((data[dataIndex] - 272.15) * mod));
+            this.bitmap.data[idx + 0] = Math.abs(range + ((value - 272.15) * mod));
             this.bitmap.data[idx + 1] = 0;
             this.bitmap.data[idx + 2] = 0;
+            // }
+
+
+            // console.log('coords: ', [((lon + 180) % 360) - 180, header.la1 - (y * header.dy)]);
 
             if (x == image.bitmap.width - 1 &&
                 y == image.bitmap.height - 1) {
